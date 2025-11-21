040e2405ab7b95cf739d5c15bb4fa0da21034ac4
DollarNoob <35988515+DollarNoob@users.noreply.github.com>
2025-11-22 03:35:10 +0900

    Hotfix for context menu not appearing

--- src/utils/menuIcon.ts
+++ src/utils/menuIcon.ts
@@ -1,10 +1,11 @@
 import { Image } from "@tauri-apps/api/image";
+import { resolveResource } from "@tauri-apps/api/path";
 
 const iconCache: Record<string, Image> = {};
 
 export async function useMenuIcon(path: string) {
     if (!iconCache[path]) {
-        iconCache[path] = await Image.fromPath("icons/" + path);
+        iconCache[path] = await Image.fromPath(await resolveResource("icons/" + path));
     }
     return iconCache[path];
 }
